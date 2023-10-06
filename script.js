97e1bb5a43b403babf99e55eddb4a15287b3e806
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-10-06 21:07:11 +0800

    myportfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// var typingEffect = new Typed(".multitext",{
+//   strings : ["Coder", "Programmer","Developer","Designer"],
+//   loop : true,
+//   typeSpeed : 100,
+//   backSpeed : 80,
+//   backDelay : 1500
+// })
